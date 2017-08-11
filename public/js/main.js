36331b9e61fbc1a0563685f7744f4fa725ca3363
Jason Portilla <jason.portilla85@gmail.com>
2017-08-11 03:03:03 +0000

    added landing page with sign up form

--- public/js/main.js
+++ public/js/main.js
@@ -12,12 +12,14 @@ $(".view-client-button").on("click", function(event){
     
     //get the right client information when button clicked
     var viewClient = $(this).data("id");
+    
+    //ajax call to route to get information from the server and display content on screen
       $.ajax({
       url: "/clients/" + viewClient,
       method: "GET"
     }).done(function(response) {
+        
       //get the remaining balance from client
-      console.log(response._id);
        response.balance = clientBalance(response.total_fee, response.down_payment);
       
        //when user clicks on view client button, HTML is displayed
@@ -55,27 +57,53 @@ $(".view-client-button").on("click", function(event){
                       <h5>Court Case Number</h5>\
                     </div>';
                     
+        //display the content on screen           
         $("#display-data").html(details);
         
+        //adding edit button when the user clicks on view and they want to edit client information
         var editButton = $("<button>").addClass("btn btn-lg btn-warning edit-client-button").text("Edit Client");
         $(".edit-buttons").html(editButton);
         
+        //when edit button is clicked, the url will take them to the edit page to make changes
         $(".edit-client-button").on("click", function(){
            window.location.href='/clients/' + response._id + '/edit'; 
-        });
-    });
+        });//end edit click button function
+        
+    }); //end promise function
+    
+});//end view client click button
+
+$('.option a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  var target = $(this).attr('href');
+
+//   $('.tab-content > div').not(target).hide();
+  
+//   $(target).fadeIn(600);
+  
 });
 
-});
+});//end document.ready
 
 
-
+//function that converts number into currency format
 function format1(n, currency) {
     return currency + "" + n.toFixed(2).replace(/./g, function(c, i, a) {
         return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
     });
 }
 
+//function that calculates the initial balance of client with down payment if there is one
 function clientBalance(totalFee, down) {
   return totalFee - down;
+}
+
+//function that calculates balance after a payment is made by client
+function payment(totalFee, clientPayment){
+    return totalFee - clientPayment;
 }
