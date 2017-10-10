c68278b183dc57a8100d39697962af3fe12b1cd3
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-10-10 14:46:29 -0400

    app completed

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ var express         = require("express"),
     engine            = require('ejs-locals'),
     bodyParser      = require("body-parser"),
     methodOverride  = require("method-override"),
-    User            = require("./models/user"),
-    braintree       = require("braintree");
-
-var gateway = braintree.connect({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "f2wz6j8grwtmwwpj",
-  publicKey: "6q7j4pvbp92s6w36",
-  privateKey: "a3b380c03f83622faaee9f96bfacaf0f"
-});
+    User            = require("./models/user");
 
 const port = process.env.PORT || 3000;
 
--- public/js/main.js
+++ public/js/main.js
@@ -104,67 +104,6 @@ var options = {
 var clientList = new List('client-names', options);
 
 
-var form = document.querySelector('#cardForm');
-var authorization = 'sandbox_fvkg8dsh_f2wz6j8grwtmwwpj';
-
-braintree.client.create({
-  authorization: authorization
-}, function(err, clientInstance) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  createHostedFields(clientInstance);
-});
-
-function createHostedFields(clientInstance) {
-  braintree.hostedFields.create({
-    client: clientInstance,
-    styles: {
-      'input': {
-        'font-size': '16px',
-        'font-family': 'courier, monospace',
-        'font-weight': 'lighter',
-        'color': '#ccc'
-      },
-      ':focus': {
-        'color': 'black'
-      },
-      '.valid': {
-        'color': '#8bdda8'
-      }
-    },
-    fields: {
-      number: {
-        selector: '#card-number',
-        placeholder: '4111 1111 1111 1111'
-      },
-      cvv: {
-        selector: '#cvv',
-        placeholder: '123'
-      },
-      expirationDate: {
-        selector: '#expiration-date',
-        placeholder: 'MM/YYYY'
-      },
-      postalCode: {
-        selector: '#postal-code',
-        placeholder: '11111'
-      }
-    }
-  }, function (err, hostedFieldsInstance) {
-    var teardown = function (event) {
-      event.preventDefault();
-      alert('Submit your nonce to your server here!');
-      hostedFieldsInstance.teardown(function () {
-        createHostedFields(clientInstance);
-        form.removeEventListener('submit', teardown, false);
-      });
-    };
-
-    form.addEventListener('submit', teardown, false);
-  });
-}
 
 //========================== FUNCTIONS SECTION =============================
 
